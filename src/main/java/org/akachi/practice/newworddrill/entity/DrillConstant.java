8fb3a6718f7a24fa579d0c540fc1ea8a0efaa1c8
akachi <zsts@hotmail.com>
2020-12-02 14:01:31 +0800

    ?! 写入出现BUG

--- src/main/java/org/akachi/practice/newworddrill/entity/DrillConstant.java
+++ src/main/java/org/akachi/practice/newworddrill/entity/DrillConstant.java
@@ -18,6 +18,6 @@ public class DrillConstant {
     public static final String AUDIO_SUFFIX = ".mp3";
     public static final String DIAGONAL = "/";
     public static final String UNDERLINE = "_";
-    public static final Integer AUDIO_TYPE_COUNT = 5;
+    public static final Integer AUDIO_TYPE_COUNT = 2;
 
 }
